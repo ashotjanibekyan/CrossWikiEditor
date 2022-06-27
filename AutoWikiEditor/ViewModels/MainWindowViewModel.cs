a8621260b4c3f6dc6bc9be9e9b2ca8658d741f35
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2022-06-27 13:14:28 +0400

    Close the app when clicked on Exit.

--- AutoWikiEditor/ViewModels/MainWindowViewModel.cs
+++ AutoWikiEditor/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace AutoWikiEditor.ViewModels
+﻿using System.Reactive;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace AutoWikiEditor.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        public MainWindowViewModel()
+        {
+            this.ExitCommand = ReactiveCommand.Create<Window>(this.Exit);
+        }
+
+        public ReactiveCommand<Window, Unit> ExitCommand { get; }
+
+        private void Exit(Window window)
+        {
+            window.Close();
+        }
     }
 }
