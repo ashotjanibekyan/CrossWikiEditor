cc87001135f4b001ee4118a8803c2503cee03b09
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-08-08 03:35:26 +0400

    Add StatusBarViewModelTests

---
+++ CrossWikiEditor.Tests/ViewModels/StatusBarViewModelTests.cs
@@ -0,0 +1,39 @@
+﻿using CrossWikiEditor.Models;
+using CrossWikiEditor.Repositories;
+using CrossWikiEditor.Services;
+using CrossWikiEditor.ViewModels;
+using Moq;
+
+namespace CrossWikiEditor.Tests.ViewModels;
+
+public class StatusBarViewModelTests
+{
+    private readonly Mock<IDialogService> _dialogServiceMock = new();
+    private readonly Mock<IProfileRepository> _profileRepositoryMock = new();
+    private readonly Mock<ICredentialService> _credentialServiceMock = new();
+    private StatusBarViewModel? _statusBarViewModel;
+    
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _statusBarViewModel = new StatusBarViewModel(_dialogServiceMock.Object, _profileRepositoryMock.Object, _credentialServiceMock.Object);
+
+    }
+
+    [Test]
+    public void UsernameClickedCommand_OpensProfilesWindow()
+    {
+        // arrange
+        _profileRepositoryMock
+            .Setup(profileService => profileService.GetAll())
+            .Returns(new List<Profile>());
+        
+        // act
+        _statusBarViewModel?.UsernameClickedCommand.Execute().Subscribe();
+
+        // assert
+        _dialogServiceMock
+            .Verify(dialogService => dialogService.ShowDialog<bool>(It.IsAny<ProfilesViewModel>()));
+    }
+}
