8239e50953361aae1cc36f1cc91aa71ad6a9b391
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-08-13 21:39:49 +0400

    Fix Preferences.General section

--- CrossWikiEditor/ViewModels/PreferencesViewModel.cs
+++ CrossWikiEditor/ViewModels/PreferencesViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace CrossWikiEditor.ViewModels;
+﻿using ReactiveUI.Fody.Helpers;
+
+namespace CrossWikiEditor.ViewModels;
 
 public class PreferencesViewModel : ViewModelBase
 {
-    
+    [Reactive]
+    public bool MinimizeToSystray { get; set; }
+
+    [Reactive]
+    public bool WarnOnExit { get; set; }
+
+    [Reactive]
+    public bool SavePageListWithSettings { get; set; }
+
+    [Reactive]
+    public bool LowThreadPriority { get; set; }
+
+    [Reactive]
+    public bool PreviewDiffInBotMode { get; set; }
+
+    [Reactive]
+    public bool EnableLogging { get; set; }
 }
