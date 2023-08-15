5b8da9573d75680f3484cd73a1db70b2cc2f5851
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-08-15 23:57:37 +0400

    Add Alerts class for PreferencesViewModel

--- CrossWikiEditor/ViewModels/PreferencesViewModel.cs
+++ CrossWikiEditor/ViewModels/PreferencesViewModel.cs
@@ -6,6 +6,17 @@ namespace CrossWikiEditor.ViewModels;
 
 public class PreferencesViewModel : ViewModelBase
 {
+    public PreferencesViewModel()
+    {
+        Alerts = new()
+        {
+            AmbiguousCitationDates = true,
+            EditorsSignatureOrLink = true,
+            DeadLinks = true,
+            MultipleDefaultSort = true,
+            SeeAlsoOutOfPlace = true
+        };
+    }
     [Reactive]
     public bool MinimizeToSystray { get; set; }
 
@@ -56,4 +67,76 @@ public class PreferencesViewModel : ViewModelBase
     
     [Reactive]
     public bool EmptyPageListOnProjectChange { get; set; }
+
+    [Reactive]
+    public Alerts Alerts { get; set; }
+}
+
+public class Alerts
+{
+    [Reactive]
+    public bool AmbiguousCitationDates { get; set; }
+    
+    [Reactive]
+    public bool ContainsSicTag { get; set; }
+    
+    [Reactive]
+    public bool DabPageWithRef { get; set; }
+    
+    [Reactive]
+    public bool DeadLinks { get; set; }
+    
+    [Reactive]
+    public bool DuplicateParametersInWpBannerShell { get; set; }
+    
+    [Reactive]
+    public bool HasRefAfterReferences { get; set; }
+    
+    [Reactive]
+    public bool HasFootnotesTemplate { get; set; }
+    
+    [Reactive]
+    public bool HeadersWithWikilinks { get; set; }
+    
+    [Reactive]
+    public bool InvalidCitationParameters { get; set; }
+    
+    [Reactive]
+    public bool LinksWithDoublePipes { get; set; }
+    
+    [Reactive]
+    public bool LinksWithNoTarget { get; set; }
+    
+    [Reactive]
+    public bool LongArticleWithStubTag { get; set; }
+    
+    [Reactive]
+    public bool MultipleDefaultSort { get; set; }
+    
+    [Reactive]
+    public bool NoCategory { get; set; }
+    
+    [Reactive]
+    public bool SeeAlsoOutOfPlace { get; set; }
+    
+    [Reactive]
+    public bool StartsWithHeading { get; set; }
+    
+    [Reactive]
+    public bool UnbalancedBrackets { get; set; }
+    
+    [Reactive]
+    public bool UnclosedTags { get; set; }
+    
+    [Reactive]
+    public bool UnformattedReferences { get; set; }
+    
+    [Reactive]
+    public bool UnknownParametersInMultipleIssues { get; set; }
+    
+    [Reactive]
+    public bool UnknownParametersInWpBannerShell { get; set; }
+    
+    [Reactive]
+    public bool EditorsSignatureOrLink { get; set; }
 }
