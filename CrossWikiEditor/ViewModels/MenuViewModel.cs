067e84e2140254a553cdafe4b140c9c7eec81a39
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-08-23 23:50:50 +0400

    Add additional command for File menu.

--- CrossWikiEditor/ViewModels/MenuViewModel.cs
+++ CrossWikiEditor/ViewModels/MenuViewModel.cs
@@ -1,5 +1,4 @@
 using System.Reactive;
-using System.Threading.Tasks;
 using Avalonia.Controls;
 using ReactiveUI;
 
@@ -12,13 +11,70 @@ public class MenuViewModel : ViewModelBase
     public MenuViewModel(Window mainWindow)
     {
         _mainWindow = mainWindow;
-        ExitCommand = ReactiveCommand.Create(this.Exit);
+        ResetToDefaultSettingsCommand = ReactiveCommand.Create(ResetToDefaultSettings);
+        OpenSettingsCommand = ReactiveCommand.Create(OpenSettings);
+        SaveSettingsCommand = ReactiveCommand.Create(SaveSettings);
+        SaveSettingsAsCommand = ReactiveCommand.Create(SaveSettingsAs);
+        SaveSettingsAsDefaultCommand = ReactiveCommand.Create(SaveSettingsAsDefault);
+        LoginProfilesCommand = ReactiveCommand.Create(LoginProfiles);
+        LogoutCommand = ReactiveCommand.Create(Logout);
+        RefreshStatusAndTyposCommand = ReactiveCommand.Create(RefreshStatusAndTypos);
+        ExitCommand = ReactiveCommand.Create(Exit);
     }
 
-    public void Exit()
+    private void ResetToDefaultSettings()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void OpenSettings()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void SaveSettings()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void SaveSettingsAs()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void SaveSettingsAsDefault()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void LoginProfiles()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Logout()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void RefreshStatusAndTypos()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Exit()
     {
         _mainWindow.Close();
     }
     
-    public ReactiveCommand<Unit, Unit> ExitCommand { get; set; }
+    
+    public ReactiveCommand<Unit, Unit> ResetToDefaultSettingsCommand { get; init; }
+    public ReactiveCommand<Unit, Unit> OpenSettingsCommand { get; init; }
+    public ReactiveCommand<Unit, Unit> SaveSettingsCommand { get; init; }
+    public ReactiveCommand<Unit, Unit> SaveSettingsAsCommand { get; init; }
+    public ReactiveCommand<Unit, Unit> SaveSettingsAsDefaultCommand { get; init; }
+    public ReactiveCommand<Unit, Unit> LoginProfilesCommand { get; init; }
+    public ReactiveCommand<Unit, Unit> LogoutCommand { get; init; }
+    public ReactiveCommand<Unit, Unit> RefreshStatusAndTyposCommand { get; init; }
+    public ReactiveCommand<Unit, Unit> ExitCommand { get; init; }
 }
