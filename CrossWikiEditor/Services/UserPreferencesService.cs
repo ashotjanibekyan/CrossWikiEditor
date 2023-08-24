4d1b5b46dcf349caae721672c92bd51e74fac02d
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-08-24 04:35:10 +0400

    Update currentUserPref when it is changed from the UI per LanguageCodeChangedMessage and ProjectChangedMessage

--- CrossWikiEditor/Services/UserPreferencesService.cs
+++ CrossWikiEditor/Services/UserPreferencesService.cs
@@ -3,25 +3,41 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Serialization;
+using CrossWikiEditor.Messages;
+using ReactiveUI;
 
 namespace CrossWikiEditor.Services;
 
 public interface IUserPreferencesService
 {
     UserPrefs GetUserPref(string path);
+    UserPrefs GetCurrentPref();
 }
-
 
 public sealed class UserPreferencesService : IUserPreferencesService
 {
+    private UserPrefs _currentPref;
+
+    public UserPreferencesService()
+    {
+        _currentPref = new UserPrefs();
+
+        MessageBus.Current.Listen<LanguageCodeChangedMessage>()
+            .Subscribe(m => _currentPref.LanguageCode = m.LanguageCode);
+        MessageBus.Current.Listen<ProjectChangedMessage>()
+            .Subscribe(m => _currentPref.Project = m.Project);
+    }
+    
     public UserPrefs GetUserPref(string path)
     {
-        string settings = File.ReadAllText(path, Encoding.UTF8);
-
-
+        var settings = File.ReadAllText(path, Encoding.UTF8);
         settings = Regex.Replace(settings, @"<(/?)\s*SourceIndex>", "<$1SelectedProvider>");
-
-        XmlSerializer xs = new XmlSerializer(typeof(UserPrefs));
+        var xs = new XmlSerializer(typeof(UserPrefs));
         return (UserPrefs)xs.Deserialize(new StringReader(settings));
+    }
+
+    public UserPrefs GetCurrentPref()
+    {
+        return _currentPref;
     }
 }
