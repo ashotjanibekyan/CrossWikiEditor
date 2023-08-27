4e6f265a82a5d2d4d55c1aeb5d9ab4ce85cfa6a9
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-08-27 22:08:44 +0400

    Add WikiSiteExtensions.cs

---
+++ CrossWikiEditor/WikiClientLibraryUtils/WikiSiteExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using WikiClientLibrary.Client;
+using WikiClientLibrary.Sites;
+
+namespace CrossWikiEditor.WikiClientLibraryUtils;
+
+public static class WikiSiteExtensions
+{
+    public static async Task<MagicWordCollection> GetMagicWords(this WikiSite wikiSite)
+    {
+        JToken? result = await wikiSite.InvokeMediaWikiApiAsync(new MediaWikiFormRequestMessage(new
+        {
+            action = "query",
+            meta = "siteinfo",
+            siprop = "magicwords"
+        }), true, CancellationToken.None);
+        return new MagicWordCollection((JArray)result["query"]["magicwords"]);
+    }
+} 
