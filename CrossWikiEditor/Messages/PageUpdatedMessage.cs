781046cd496900d61e6c8b99015357f43635adf6
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-08-29 02:42:57 +0400

    Add PageUpdatedMessage

---
+++ CrossWikiEditor/Messages/PageUpdatedMessage.cs
@@ -0,0 +1,10 @@
+﻿using CrossWikiEditor.Models;
+
+namespace CrossWikiEditor.Messages;
+
+public class PageUpdatedMessage(WikiPageModel wikiPageModel, string oldContent, string newContent)
+{
+    public WikiPageModel Page { get; } = wikiPageModel;
+    public string OldContent { get; } = oldContent;
+    public string NewContent { get; } = newContent;
+}
