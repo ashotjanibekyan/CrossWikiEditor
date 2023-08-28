dbea2fbb53cd3f62762e12eef3b1deb4127cd79c
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-08-29 03:00:33 +0400

    Start bot draft

---
+++ CrossWikiEditor/PageListProcessor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging;
+using CrossWikiEditor.Messages;
+using CrossWikiEditor.Models;
+using CrossWikiEditor.Settings;
+using WikiClientLibrary.Pages;
+
+namespace CrossWikiEditor;
+
+public class PageListProcessor
+{
+    private readonly IMessenger _messenger;
+    private readonly List<WikiPageModel> _pages;
+    private readonly NormalFindAndReplaceRules _normalFindAndReplaceRules;
+    private bool _isAlive = true;
+
+    public PageListProcessor(IMessenger messenger, List<WikiPageModel> pages, NormalFindAndReplaceRules normalFindAndReplaceRules)
+    {
+        _messenger = messenger;
+        _pages = pages;
+        _normalFindAndReplaceRules = normalFindAndReplaceRules;
+        messenger.Register<StopBotMessage>(this, (recipient, message) =>
+        {
+            _isAlive = false;
+        });
+    }
+
+    public async Task Start()
+    {
+        foreach (WikiPageModel page in _pages.TakeWhile(_ => _isAlive))
+        {
+            await ProcessPage(page);
+        }
+    }
+    
+    private async Task ProcessPage(WikiPageModel page)
+    {
+        if (page.WikiPage is null)
+        {
+            return;
+        }
+
+        if (string.IsNullOrEmpty(page.WikiPage.Content))
+        {
+            await page.WikiPage.RefreshAsync(PageQueryOptions.FetchContent);
+        }
+        string initialContent = page.WikiPage.Content;
+        string newContent = page.WikiPage.Content;
+        foreach (NormalFindAndReplaceRule normalFindAndReplaceRule in _normalFindAndReplaceRules)
+        {
+            if (!normalFindAndReplaceRule.Regex)
+            {
+                newContent = newContent.Replace(normalFindAndReplaceRule.Find, normalFindAndReplaceRule.ReplaceWith);
+            }
+            else
+            {
+                var regex = new Regex(normalFindAndReplaceRule.Find);
+                regex.Replace(newContent, normalFindAndReplaceRule.ReplaceWith);
+            }
+        }
+
+        page.WikiPage.Content = newContent;
+        try
+        {
+            await Task.Delay(1000);
+            //await page.WikiPage.UpdateContentAsync("via CWB");
+            _messenger.Send(new PageUpdatedMessage(page, initialContent, newContent));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        
+    }
+}
--- CrossWikiEditor/ViewModels/MainWindowViewModel.cs
+++ CrossWikiEditor/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,17 @@
-﻿using CrossWikiEditor.ViewModels.ControlViewModels;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging;
+using CrossWikiEditor.Messages;
+using CrossWikiEditor.ViewModels.ControlViewModels;
 using CrossWikiEditor.ViewModels.MenuViewModels;
 using CrossWikiEditor.ViewModels.ReportViewModels;
 
 namespace CrossWikiEditor.ViewModels;
 
-public sealed class MainWindowViewModel(StatusBarViewModel statusBarViewModel,
+public sealed class MainWindowViewModel : ViewModelBase
+{
+    private Task? _myBot;
+    public MainWindowViewModel(StatusBarViewModel statusBarViewModel,
         MakeListViewModel makeListViewModel,
         OptionsViewModel optionsViewModel,
         MoreViewModel moreViewModel,
@@ -16,23 +23,43 @@ public sealed class MainWindowViewModel(StatusBarViewModel statusBarViewModel,
         HistoryViewModel historyViewModel,
         WhatLinksHereViewModel whatLinksHereViewModel,
         LogsViewModel logsViewModel,
-        PageLogsViewModel pageLogsViewModel)
-    : ViewModelBase
-{
-    public StatusBarViewModel StatusBarViewModel { get; } = statusBarViewModel;
+        PageLogsViewModel pageLogsViewModel,
+        IMessenger messenger)
+    {
+        StatusBarViewModel = statusBarViewModel;
+        MakeListViewModel = makeListViewModel;
+        OptionsViewModel = optionsViewModel;
+        MoreViewModel = moreViewModel;
+        MenuViewModel = menuViewModel;
+        DisambigViewModel = disambigViewModel;
+        SkipViewModel = skipViewModel;
+        StartViewModel = startViewModel;
+        EditBoxViewModel = editBoxViewModel;
+        HistoryViewModel = historyViewModel;
+        WhatLinksHereViewModel = whatLinksHereViewModel;
+        LogsViewModel = logsViewModel;
+        PageLogsViewModel = pageLogsViewModel;
+        messenger.Register<StartBotMessage>(this, (recipient, message) =>
+        {
+            _myBot = Task.Run(async () =>
+            {
+                var pageProcessor = new PageListProcessor(messenger, MakeListViewModel.Pages.ToList(), OptionsViewModel.NormalFindAndReplaceRules);
+                await pageProcessor.Start();
+            });
+        });
+    }
 
-    public MakeListViewModel MakeListViewModel { get; } = makeListViewModel;
-    public OptionsViewModel OptionsViewModel { get; } = optionsViewModel;
-    public MoreViewModel MoreViewModel { get; } = moreViewModel;
-    public MenuViewModel MenuViewModel { get; set; } = menuViewModel;
-    public DisambigViewModel DisambigViewModel { get; } = disambigViewModel;
-    public SkipViewModel SkipViewModel { get; } = skipViewModel;
-    public StartViewModel StartViewModel { get; } = startViewModel;
-
-
-    public EditBoxViewModel EditBoxViewModel { get; } = editBoxViewModel;
-    public HistoryViewModel HistoryViewModel { get; } = historyViewModel;
-    public WhatLinksHereViewModel WhatLinksHereViewModel { get; } = whatLinksHereViewModel;
-    public LogsViewModel LogsViewModel { get; } = logsViewModel;
-    public PageLogsViewModel PageLogsViewModel { get; } = pageLogsViewModel;
+    public StatusBarViewModel StatusBarViewModel { get; }
+    public MakeListViewModel MakeListViewModel { get; }
+    public OptionsViewModel OptionsViewModel { get; }
+    public MoreViewModel MoreViewModel { get; }
+    public MenuViewModel MenuViewModel { get; set; }
+    public DisambigViewModel DisambigViewModel { get; }
+    public SkipViewModel SkipViewModel { get; }
+    public StartViewModel StartViewModel { get; }
+    public EditBoxViewModel EditBoxViewModel { get; }
+    public HistoryViewModel HistoryViewModel { get; }
+    public WhatLinksHereViewModel WhatLinksHereViewModel { get; }
+    public LogsViewModel LogsViewModel { get; }
+    public PageLogsViewModel PageLogsViewModel { get; }
 }
