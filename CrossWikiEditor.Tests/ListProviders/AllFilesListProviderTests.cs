5e3283bc35aa8f4acaa719d6bc4b85baa65ec662
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-09-09 02:35:09 +0400

    Add AllFilesListProviderTests and AllCategoriesListProviderTests

---
+++ CrossWikiEditor.Tests/ListProviders/AllFilesListProviderTests.cs
@@ -0,0 +1,73 @@
+using CrossWikiEditor.Core.ListProviders;
+using CrossWikiEditor.Core.Models;
+using CrossWikiEditor.Core.Settings;
+using CrossWikiEditor.Core.Utils;
+
+namespace CrossWikiEditor.Tests.ListProviders;
+
+public class AllFilesListProviderTests : BaseTest
+{
+    private AllFilesListProvider _sut;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        SetUpServices();
+        _sut = new AllFilesListProvider(_dialogService, _pageService, _userPreferencesService);
+    }
+
+    [Test]
+    public async Task MakeList_ShouldReturnPageServiceResults()
+    {
+        // arrange
+        var expectedPages = new List<WikiPageModel>
+        {
+            new("title", "https://hyw.wikipedia.org/w/api.php?", _wikiClientCache),
+            new("title1", "https://hyw.wikipedia.org/w/api.php?", _wikiClientCache),
+        };
+        _pageService.GetAllFiles("https://hyw.wikipedia.org/w/api.php?", "", 73)
+            .Returns(Result<List<WikiPageModel>>.Success(expectedPages));
+        _userPreferencesService.GetCurrentPref().Returns(new UserPrefs
+        {
+            LanguageCode = "hyw",
+            Project = ProjectEnum.Wikipedia
+        });
+
+        // act
+        Result<List<WikiPageModel>> result = await _sut.MakeList(73);
+
+        // assert
+        result.IsSuccessful.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value[0].Title.Should().Be("title");
+        result.Value[1].Title.Should().Be("title1");
+    }
+
+    [Test]
+    public async Task MakeList_ShouldReturnUnsuccessfulResult_WhenPageServiceReturnsUnsuccessfulResult()
+    {
+        // arrange
+        _pageService.GetAllFiles("https://hyw.wikipedia.org/w/api.php?", "", 73)
+            .Returns(Result<List<WikiPageModel>>.Failure("failed to get pages"));
+        _userPreferencesService.GetCurrentPref().Returns(new UserPrefs
+        {
+            LanguageCode = "hyw",
+            Project = ProjectEnum.Wikipedia
+        });
+
+        // act
+        Result<List<WikiPageModel>> result = await _sut.MakeList(73);
+
+        // assert
+        result.IsSuccessful.Should().BeFalse();
+        result.Error.Should().Be("failed to get pages");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _sut.CanMake.Should().BeTrue();
+        _sut.Title.Should().Be("All Files");
+        _sut.ParamTitle.Should().Be("Start from");
+    }
+}
