5f97ae945fbdfa3dae69a584c8641c6041409bb9
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-09-09 16:26:40 +0400

    +NumberExtensionsTests

---
+++ CrossWikiEditor.Tests/Utils/NumberExtensionsTests.cs
@@ -0,0 +1,28 @@
+namespace CrossWikiEditor.Tests.Utils;
+
+public class NumberExtensionsTests
+{
+    [TestCase(0, ExpectedResult = true)]
+    [TestCase(-2, ExpectedResult = true)]
+    [TestCase(2, ExpectedResult = true)]
+    [TestCase(-10, ExpectedResult = true)]
+    [TestCase(10, ExpectedResult = true)]
+    [TestCase(1, ExpectedResult = false)]
+    [TestCase(-1, ExpectedResult = false)]
+    [TestCase(3, ExpectedResult = false)]
+    [TestCase(-11, ExpectedResult = false)]
+    [TestCase(11, ExpectedResult = false)]
+    public bool IsEven(int value) => value.IsEven();
+    
+    [TestCase(0, ExpectedResult = false)]
+    [TestCase(-2, ExpectedResult = false)]
+    [TestCase(2, ExpectedResult = false)]
+    [TestCase(-10, ExpectedResult = false)]
+    [TestCase(10, ExpectedResult = false)]
+    [TestCase(1, ExpectedResult = true)]
+    [TestCase(-1, ExpectedResult = true)]
+    [TestCase(3, ExpectedResult = true)]
+    [TestCase(-11, ExpectedResult = true)]
+    [TestCase(11, ExpectedResult = true)]
+    public bool IsOdd(int value) => value.IsOdd();
+}
