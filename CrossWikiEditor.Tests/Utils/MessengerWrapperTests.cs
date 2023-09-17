1661972f1d81613bef50f49cc8a6488e6e8623c3
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-09-17 15:49:23 +0400

    Add MessengerWrapperTests

---
+++ CrossWikiEditor.Tests/Utils/MessengerWrapperTests.cs
@@ -0,0 +1,39 @@
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace CrossWikiEditor.Tests.Utils;
+
+public class MessengerWrapperTests
+{
+    [Test]
+    public void Send_ShouldInvokeMessengerSendMethod()
+    {
+        // Arrange
+        IMessenger? mockMessenger = Substitute.For<IMessenger>();
+        var wrapper = new MessengerWrapper(mockMessenger);
+        var message = new StartBotMessage();
+        mockMessenger.Send(message).Returns(message);
+
+        // Act
+        StartBotMessage result = wrapper.Send(message);
+
+        // Assert
+        mockMessenger.Received(1).Send(message);
+        result.Should().Be(message);
+    }
+
+    [Test]
+    public void Register_ShouldInvokeMessengerRegisterMethod()
+    {
+        // Arrange
+        IMessenger? mockMessenger = Substitute.For<IMessenger>();
+        var wrapper = new MessengerWrapper(mockMessenger);
+        var recipient = new object();
+        MessageHandler<object, StartBotMessage> handler = (sender, args) => { };
+
+        // Act
+        wrapper.Register(recipient, handler);
+
+        // Assert
+        mockMessenger.Received(1).Register(recipient, handler);
+    }
+}
