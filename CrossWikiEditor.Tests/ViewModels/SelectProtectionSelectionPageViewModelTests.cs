1d5206cb42212dc9a95eef6952a67cee4410361c
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-09-17 19:04:57 +0400

    Add SelectProtectionSelectionPageViewModelTests

---
+++ CrossWikiEditor.Tests/ViewModels/SelectProtectionSelectionPageViewModelTests.cs
@@ -0,0 +1,31 @@
+namespace CrossWikiEditor.Tests.ViewModels;
+
+public class SelectProtectionSelectionPageViewModelTests : BaseTest
+{
+    private SelectProtectionSelectionPageViewModel _sut;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        SetUpServices();
+        _sut = new SelectProtectionSelectionPageViewModel();
+    }
+
+    [TestCase(0, "edit", 3, "")]
+    [TestCase(1, "move", 1, "sysop")]
+    [TestCase(2, "edit|move", 2, "autoconfirmed|sysop")]
+    [TestCase(3, "", 0, "autoconfirmed")]
+    public void OkCommand_ShouldReturnTypeAndLevel(int type, string expectedType, int level, string expectedLevel)
+    {
+        // arrange
+        _sut.ProtectionType = type;
+        _sut.ProtectionLevel = level;
+        IDialog dialog = Substitute.For<IDialog>();
+        
+        // act
+        _sut.OkCommand.Execute(dialog);
+
+        // assert
+        dialog.Received(1).Close(Arg.Is<(string, string)>(tpl => tpl.Item1 == expectedType && tpl.Item2 == expectedLevel));
+    }
+}
