b6a4965563334866cc85c18090ef8169974572a7
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2023-11-28 22:25:34 +0400

    Migrate to .net8

--- CrossWikiEditor.Tests/Usings.cs
+++ CrossWikiEditor.Tests/Usings.cs
@@ -1,3 +1,10 @@
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Threading;
+global using System.Threading.Tasks;
 global using CrossWikiEditor.Core;
 global using FluentAssertions;
 global using NSubstitute;
