6ad3334dee9c0baa9c25f69d65b678a762cf1803
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-05-17 23:34:46 +0400

    +MoreOptions

---
+++ src/CrossWikiEditor.Core/Settings/MoreOptions.cs
@@ -0,0 +1,30 @@
+﻿namespace CrossWikiEditor.Core.Settings;
+
+/// <summary>
+/// Corresponds to the <see cref="MoreViewModel"/>
+/// </summary>
+public sealed class MoreOptions
+{
+    public bool AppendPrependEnabled { get; set; }
+    public bool IsAppend { get; set; }
+    public string AppendOrPrependText { get; set; } = string.Empty;
+    public int UseNNewlines { get; set; }
+    public bool SortMetaDataAfterAppendOrPrepend { get; set; }
+    public List<FilesOrCategoryAction> FileActions { get; set; } = [];
+    public List<FilesOrCategoryAction> CategoryActions { get; set; } = [];
+}
+
+public enum ActionType
+{
+    Replace,
+    Remove,
+    Add,
+    CommentOut
+}
+public sealed class FilesOrCategoryAction(ActionType actionType)
+{
+    public ActionType Type { get; set; } = actionType;
+    public bool SkipIfNotChanged { get; set; }
+    public string? Value1 { get; set; }
+    public string? Value2 { get; set; }
+}
--- src/CrossWikiEditor.Core/Settings/UserSettings.cs
+++ src/CrossWikiEditor.Core/Settings/UserSettings.cs
@@ -5,6 +5,7 @@ public sealed class UserSettings
     public UserWiki UserWiki { get; set; } = new UserWiki("hy", ProjectEnum.Wikipedia);
     public SkipOptions SkipOptions { get; set; } = new();
     public GeneralOptions GeneralOptions { get; set; } = new();
+    public MoreOptions MoreOptions { get; set; } = new();
     public bool IsBotMode { get; set; }
     public string DefaultSummary { get; set; } = string.Empty;
     public string GetApiUrl() => UserWiki.GetApiUrl();
